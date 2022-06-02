3a9822bc42533a0588ad519b68dc7552ed6e8dbe
Paul Chapdelaine <paulchapdelaine@gmail.com>
2022-06-02 11:31:52 +0200

    Exo 3 finis

--- exoJose3/vue-project/src/App.vue
+++ exoJose3/vue-project/src/App.vue
@@ -1,12 +1,43 @@
 <script setup>
 import { ref } from "vue";
+import * as jose from 'jose'
 
 const compteur = ref(0);
 
-setInterval(() => {
+//setInterval(() => {
 
-  compteur.value++
-}, 1000 )
+//  compteur.value++
+//}, 1000 )
+
+function resetCompteur(){
+  compteur.value = 0;
+}
+
+const jwtInput = ref('')
+const claims = ref(null);
+
+function decodeClaims() {
+  try{
+  claims.value = jose.decodeJwt(jwtIpunt.value)
+
+} catch (err) {
+  claims.value = err.message
+  }
+}
+
+function saveJwt(){
+
+  localStorage.stItem(jwtInput.value)
+  loadSavedJwt()
+}
+
+const savedJwt = ref('')
+function loadSavedJwt() {
+  savedJwt.value = localStorage.getItem('jwt');
+}
+
+loadSavedJwt()
+
 
 </script>
 
@@ -18,4 +49,15 @@ setInterval(() => {
     <button v-on:click="reset()">reset </button>
     <input type="number" v-model="compteur">
 
+    <hr>
+    
+    <input type="number" v-model="jwtInput">
+    <br>
+    <p>{{jwtInput}}</p>
+
+    <button @click="">Afficher les Claims</button>
+    <p>{{Claims}}</p>
+
   </template>
+
+  <style></style>
