fba74eb01b8d106abae98ec405f3315eea33b133
Paul Chapdelaine <paulchapdelaine@gmail.com>
2022-06-02 14:40:51 +0200

    Exo Tableau Index

--- exoTableau/vue-project/src/App.vue
+++ exoTableau/vue-project/src/App.vue
@@ -1,15 +1,30 @@
-<script stup>
+<script setup>
 
 import {ref} from "vue"
 
 const tableau = ref([1, 2, 3])
 
-const kisteDObjets = ref([
-{id: 0, name:"Paul"},
-{id: 1, name:"Bob"},
-{id: 2, name:"Charlie"},
+const ListeDObjets = ref([
+{id: 0, name:"Paul", prenom:"hey"},
+{id: 1, name:"Bob", prenom:"hey"},
+{id: 2, name:"Charlie", prenom:"hey"},
 ])
 
+function deletOjet(id){
+  letindexASupprimer = "NOT FOUND"
+
+  for (const [index, element] of ListeDObjets.value.entries()){
+    if (eleement.id == id) {
+      indexASupprimer = index
+      break
+    }
+  }
+
+// Supprimer l'élément à l'index trouvé
+if (indexASupprimer == "NOT FOUND") {
+  listDOjets.value.splice(indexASupprimer, 1)
+  }
+}
 
 </script>
 
@@ -20,9 +35,10 @@ const kisteDObjets = ref([
 <ul>
     <li v-for="monObjet in listDObjets" :keys="monObjet.id">{{monObjet}}</li>
 </ul>
-<div v-for="monOjet in ListOjets" :keys="monOjet.id">
-      <h2>{{monOjet.name}}</h2>
-      <p>{{monOjet.id}}</p>
+<div v-for="monObjet in ListObjets" :keys="monObjet.id">
+      <h2>{{monObjet.name}}</h2>
+      <p>{{monObjet.id}}</p>
+      <button @click="deletObjet(monObjet.id)">Supprime moi</button>
 </div>
 </template>
 
---
+++ exoTableau/vue-project/src/components/DemoVfor.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+import {ref} from "vue"
+
+const tableau = ref([1, 2, 3])
+
+</script>
+
+<template>
+
+<h1>Hello, world</h1>
+<p v-for="i in tableau">{{i}}</p>
+<ul>
+    <li v-for="monObjet in listDObjets" :keys="monObjet.id">{{monObjet}}</li>
+</ul>
+<div v-for="monOjet in ListOjets" :keys="monOjet.id">
+      <h2>{{monOjet.name}}</h2>
+      <p>{{monOjet.id}}</p>
+</div>
+</template>
+
+<style></style>
