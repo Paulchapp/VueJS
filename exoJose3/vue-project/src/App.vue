a603ac0d18d9f2344526edec01f7799ab9a85699
Paul Chapdelaine <paulchapdelaine@gmail.com>
2022-06-02 11:05:42 +0200

    Rectification exo3

--- exoJose3/vue-project/src/App.vue
+++ exoJose3/vue-project/src/App.vue
@@ -1,12 +1,21 @@
 <script setup>
 import { ref } from "vue";
 
+const compteur = ref(0);
+
+setInterval(() => {
+
+  compteur.value++
+}, 1000 )
+
 </script>
 
 
   <template>
 
-    <h1>Paul App EXO3</h1>
-    
+    <h1>Paul App</h1>
+    <p> {{compteur}} </p>
+    <button v-on:click="reset()">reset </button>
+    <input type="number" v-model="compteur">
 
   </template>
