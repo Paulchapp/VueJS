8e00290250c06ae97ebba33321f8216023693eb7
Paul Chapdelaine <paulchapdelaine@gmail.com>
2022-06-02 15:22:51 +0200

    Exo avec boostrap

---
+++ exoForms/vue-project/src/components/DemoVfor.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+import {ref} from "vue"
+
+const user = ref(null)
+
+</script>
+
+<template>
+
+<h1>Hello, world</h1>
+<hr>
+<h2>Hello Anonyme</h2>
+<h2>Hello {{user}}</h2>
+<button>se connecter</button>
+<button>se déconnecter</button>
+
+</template>
+
+<style></style>
