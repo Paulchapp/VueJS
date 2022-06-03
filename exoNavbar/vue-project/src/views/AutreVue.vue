5626378e85e12d1545808a0388f25b7edff50397
Paul Chapdelaine <paulchapdelaine@gmail.com>
2022-06-03 12:37:02 +0200

    Implémentation des Vue

---
+++ exoNavbar/vue-project/src/views/AutreVue.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="maPage">
+    <h1>Ma page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
